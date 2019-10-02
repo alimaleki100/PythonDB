286e5214950f5c4ca9a0f864a4899f9e9eee3166
Ali Maleki <ali.maleki100@gmail.com>
2019-10-02 10:58:11 +0330

    Update excel_mssqlserver.py

--- excel_mssqlserver.py
+++ excel_mssqlserver.py
@@ -4,14 +4,16 @@ Created on Thu Feb  1 01:02:26 2018
 
 @author: acer
 """
-
+#import libraries
 import pandas as pd
 import pyodbc 
 from sqlalchemy import create_engine
 
+#input file
 fileAddress='D:/ClosingStockMetaData2.xlsx'
 sheetN='sheetName'
 indexCol='indexCol'
+#destination table name
 TblName='TblName'
 df = pd.read_excel(fileAddress,sheetname=sheetN)
 engine = create_engine("mssql+pyodbc://USER:PWD@SERVER:PORT3(1433)/DBName?driver=SQL Server")
