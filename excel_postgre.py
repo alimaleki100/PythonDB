e3fee9ab6f1591b1fa22223242cb57873f6081b6
Ali Maleki <ali.maleki100@gmail.com>
2020-05-09 02:46:21 +0430

    Update excel_postgre.py

--- excel_postgre.py
+++ excel_postgre.py
@@ -2,16 +2,16 @@
 """
 Created on Thu Feb  1 01:02:26 2018
 
-@author: acer
+@author: alimaleki100
 """
-
+## Write dataframe to postgres table
 import pandas as pd
 df = pd.read_excel('file Address')
-#df.columns = [c.lower() for c in df.columns] #postgres doesn't like capitals or spaces
 
 from sqlalchemy import create_engine
 engine = create_engine('postgresql://user:PWD@SERVERNAME/DBName')
-
 df.to_sql("TBLName", engine,if_exists='append')
+
+
 
 
